5e1a9c18613b866e75c479b98a746609eb614a88
YogeshKumar <79000859+kumarYogesh20@users.noreply.github.com>
2023-02-11 15:16:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".mob-menu").click(function(){
+      $(".mob-menu-list").toggleClass("show");
+    });
+});
+
+
+// /* ------SCROLL REVEAL ANIMATION------ */
+// const sr = ScrollReveal({
+//   origin: 'top',
+//   distance: '2rem',
+//   duration: 2000,
+//   reset: true
+// })
+
+// sr.reveal('.helping-company',{})
+// sr.reveal('.heading',{delay:200})
+// sr.reveal('.sub-heading',{interval:400})
+
+// sr.reveal('.trusted-partner',{})
+// sr.reveal('.trusted-partner-icon',{delay: 400})
+// sr.reveal('.partner-logo',{interval: 200})
+
+
+// sr.reveal('.work-by-us',{})
+// sr.reveal('.col-lg-6',{interval:200})
